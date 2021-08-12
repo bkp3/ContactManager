dd86725aec03007b52522d711f8054e1e2f78178
bkp3 <bholakumar.7372@gmail.com>
2021-08-12 06:59:39 +0530

    setting up Bootstrap and Templates

--- SmartContactManager/src/main/java/com/smart/controller/HomeController.java
+++ SmartContactManager/src/main/java/com/smart/controller/HomeController.java
@@ -2,9 +2,12 @@ package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
@@ -12,24 +15,21 @@ import com.smart.entities.User;
 @Controller
 public class HomeController {
 
-	@Autowired
-	private UserRepository userRepository;
+	@RequestMapping("/home")
+	public String home(Model m) {
 
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
+		m.addAttribute("title", "Home - Smart Contact Manager");
 
-		User user = new User();
-		user.setName("Rakesh Kumar");
-		user.setEmail("rakesh@gmail.com");
-		
-		Contact c = new Contact();
-		user.getContacts().add(c);
-		
+		return "home";
+	}
+	
+	
+	@RequestMapping("/about")
+	public String about(Model m) {
 
-		userRepository.save(user);
+		m.addAttribute("title", "About - Smart Contact Manager");
 
-		return "working";
+		return "about";
 	}
 
 }
