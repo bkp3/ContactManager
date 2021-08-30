654b1e7fb34c25d8875a5a9a455ef8f58e2fd21f
Bhola Kumar <bholakumar.7372@gmail.com>
2021-08-30 17:22:28 +0530

    Server Side Validation on Registration

--- SmartContactManager/src/main/java/com/smart/controller/HomeController.java
+++ SmartContactManager/src/main/java/com/smart/controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.smart.controller;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,7 +53,7 @@ public class HomeController {
 
 	// handler for registering user
 	@PostMapping("/do_register")
-	public String registerUser(@ModelAttribute("user") User user,
+	public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result1,
 			@RequestParam(value = "agreement", defaultValue = "false") boolean agreement, Model model,
 			HttpSession session) {
 
@@ -59,6 +61,12 @@ public class HomeController {
 			if (!agreement) {
 				System.out.println("you have not agreed terms and condition");
 				throw new Exception("you have not agreed terms and condition");
+			}
+
+			if (result1.hasErrors()) {
+				System.out.println("ERROR:- " + result1.toString());
+				model.addAttribute("user", user);
+				return "signup";
 			}
 
 			user.setRole("ROLE_USER");
--- SmartContactManager/src/main/java/com/smart/entities/User.java
+++ SmartContactManager/src/main/java/com/smart/entities/User.java
@@ -13,6 +13,9 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
 @Table(name = "user")
@@ -22,6 +25,8 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 
+	@NotBlank(message = "Name field is required!!")
+	@Size(min = 2, max = 20, message = "min=2 and max=20 characters are allowed!!")
 	private String name;
 
 	@Column(unique = true)
