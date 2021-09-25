a2dabc15772e4d49def0544c252267a84327da98
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-25 07:25:44 +0530

    updated

--- SmartContactManager/src/main/java/com/smart/controller/UserController.java
+++ SmartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -1,15 +1,30 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping("/index")
-	public String dashBoard() {
+	@Autowired
+	private UserRepository userRepository;
 
+	@RequestMapping("/index")
+	public String dashBoard(Model model, Principal principal) {
+		String username = principal.getName();
+		System.out.println("username:- " + username);
+
+		// get the user by username
+		User user = this.userRepository.getUserByUserName(username);
+		System.out.println(user);
 		return "normal/user_dashboard";
 	}
 
