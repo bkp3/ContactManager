5e8192bfb64e6d3f32764906ae72f3fc35e6c826
Bhola Kumar <bholakumar.7372@gmail.com>
2021-09-27 07:35:53 +0530

    Designing Classic Add Contact Form

--- SmartContactManager/src/main/java/com/smart/controller/UserController.java
+++ SmartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -5,9 +5,12 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
+import com.smart.entities.Contact;
 import com.smart.entities.User;
 
 @Controller
@@ -17,8 +20,10 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 
-	@RequestMapping("/index")
-	public String dashBoard(Model model, Principal principal) {
+	// method for adding common data to response
+	@ModelAttribute
+	public void addCommonData(Model model, Principal principal) {
+
 		String username = principal.getName();
 		System.out.println("username:- " + username);
 
@@ -28,7 +33,22 @@ public class UserController {
 
 		model.addAttribute("user", user);
 
+	}
+
+	// dashboard home
+	@RequestMapping("/index")
+	public String dashBoard(Model model, Principal principal) {
+		model.addAttribute("title", "Home | Smart Contact Manager");
 		return "normal/user_dashboard";
+	}
+
+	// open add contact form handler
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model model) {
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+
+		return "normal/add_contact_form";
 	}
 
 }
