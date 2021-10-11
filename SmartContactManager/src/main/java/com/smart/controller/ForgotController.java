a54b5863ebd588e7d317a6afc0f851063abf3893
Bhola Kumar <bholakumar.7372@gmail.com>
2021-10-11 17:34:26 +0530

    Verify OTP and User Email while changing password

--- SmartContactManager/src/main/java/com/smart/controller/ForgotController.java
+++ SmartContactManager/src/main/java/com/smart/controller/ForgotController.java
@@ -10,13 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
 import com.smart.service.EmailService;
 
 @Controller
 public class ForgotController {
+
 	Random random = new Random(1000);
+
 	@Autowired
 	private EmailService emailService;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	// email id form open handler
 	@GetMapping("/forgot")
@@ -41,7 +48,8 @@ public class ForgotController {
 
 		boolean flag = this.emailService.sendEmail(subject, message, to);
 		if (flag) {
-			session.setAttribute("otp", otp);
+			session.setAttribute("myotp", otp);
+			session.setAttribute("email", email);
 			return "verify_otp";
 		} else {
 			session.setAttribute("message", "Check your email id...");
@@ -51,6 +59,33 @@ public class ForgotController {
 
 	}
 
+	// verify otp
+	@PostMapping("/verify-otp")
+	public String verifyOtp(@RequestParam("otp") int otp, HttpSession session) {
+		int myOtp = (int) session.getAttribute("myOtp");
+		String email = (String) session.getAttribute("email");
 
+		if (myOtp == otp) {
+			// password change form
+
+			User user = this.userRepository.getUserByUserName(email);
+			if (user == null) {
+				// send error message
+				session.setAttribute("message", "User does not exist with this email id...");
+
+				return "forgot_password";
+
+			} else {
+				// send change password form
+
+			}
+
+			return "password_change_form";
+		} else {
+
+			session.setAttribute("message", "You have entered wrong OTP");
+			return "verify_otp";
+		}
+	}
 
 }
