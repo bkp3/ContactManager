6f8ef7f924fe7c8da94757d6797bb85d77ac7ba3
Bhola Kumar <bholakumar.7372@gmail.com>
2021-10-11 09:03:11 +0530

    send OTP to email

--- SmartContactManager/src/main/java/com/smart/controller/ForgotController.java
+++ SmartContactManager/src/main/java/com/smart/controller/ForgotController.java
@@ -2,14 +2,21 @@ package com.smart.controller;
 
 import java.util.Random;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.smart.service.EmailService;
+
 @Controller
 public class ForgotController {
 	Random random = new Random(1000);
+	@Autowired
+	private EmailService emailService;
 
 	// email id form open handler
 	@GetMapping("/forgot")
@@ -19,7 +26,7 @@ public class ForgotController {
 	}
 
 	@PostMapping("/send-otp")
-	public String sendOtp(@RequestParam("email") String email) {
+	public String sendOtp(@RequestParam("email") String email, HttpSession session) {
 		System.out.println("EMAIL:- " + email);
 
 		// generating 4 digit of OTP
@@ -28,8 +35,22 @@ public class ForgotController {
 		System.out.println("OTP:- " + otp);
 
 		// write code for send otp to email..
+		String subject = "OTP from SCM";
+		String message = "Your OTP is:- " + otp;
+		String to = email;
 
-		return "verify_otp";
+		boolean flag = this.emailService.sendEmail(subject, message, to);
+		if (flag) {
+			session.setAttribute("otp", otp);
+			return "verify_otp";
+		} else {
+			session.setAttribute("message", "Check your email id...");
+
+			return "forgot_password";
+		}
+
 	}
+
+
 
 }
